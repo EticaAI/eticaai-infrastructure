9e3624f1c435ca8cb98573a6dd7c63fe0854b46d
Emerson Rocha Luiz <emerson@alligo.com.br>
2017-08-21 02:20:30 -0300

    cadu: minitube 'hello-minikube' test works

--- logbook/cadu.sh
+++ logbook/cadu.sh
@@ -12,6 +12,8 @@ sudo apt upgrade
 
 sudo hostnamectl set-hostname cadu.vps.etica.ai
 
+sudo apt install htop
+
 ## Install KVM
 # See https://github.com/kubernetes/minikube/blob/master/docs/drivers.md#kvm-driver
 sudo apt install libvirt-bin qemu-kvm
@@ -31,9 +33,36 @@ source <(kubectl completion bash)
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 
 # Install Minikube according to the instructions for the latest release.
-# CHECK NEW URL AT: https://github.com/kubernetes/minikube/releases
+# CHECK NEW VERSION AT: https://github.com/kubernetes/minikube/releases
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.21.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 ###
 
-# Start minikube
-minikube start # Error, fix it
+## Install KVM driver for docker-machine
+# See https://github.com/dhiltgen/docker-machine-kvm
+# CHECK NEW VERSION AT: https://github.com/docker/machine/releases
+curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+    chmod +x /tmp/docker-machine &&
+    sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+
+## Install docker-machine-kvm
+# CHECK NEW VERSION AT: https://github.com/dhiltgen/docker-machine-kvm/releases
+curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-ubuntu16.04 > /usr/local/bin/docker-machine-driver-kvm
+chmod +x /usr/local/bin/docker-machine-driver-kvm
+
+sudo reboot
+
+## Start minikube & basic test if is working
+# See https://kubernetes.io/docs/getting-started-guides/minikube/#quickstart
+minikube start --vm-driver=kvm
+
+# Wait... wait more... maybe 3 min
+kubectl run hello-minikube --image=gcr.io/google_containers/echoserver:1.4 --port=8080
+# > deployment "hello-minikube" created
+kubectl expose deployment hello-minikube --type=NodePort
+# > service "hello-minikube" exposed
+
+kubectl get pod # wait... until STATUS: running
+# We can see that the pod is now Running and we will now be able to curl it:
+curl $(minikube service hello-minikube --url)
+
+# minikube stop # To stop
