7e8eebe932748380f1914e9062c239d476012e4e
Emerson Rocha Luiz <emerson@alligo.com.br>
2017-08-26 02:58:55 -0300

    cadu: kubeadm installation & configuration stopped, lets try another day

--- logbook/cadu.sh
+++ logbook/cadu.sh
@@ -53,3 +53,46 @@ kubectl cluster-info # Test if enabled
 # Enabling shell autocompletion
 source <(kubectl completion bash)
 echo "source <(kubectl completion bash)" >> ~/.bashrc
+
+
+## Installing kubelet and kubeadm
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm
+
+# kubeadm init
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+### TODO check if this will blow up
+# root@cadu:~# kubeadm init
+# [kubeadm] WARNING: kubeadm is in beta, please do not use it for production clusters.
+# [init] Using Kubernetes version: v1.7.4
+# [init] Using Authorization modes: [Node RBAC]
+# [preflight] Running pre-flight checks
+# [preflight] WARNING: docker version is greater than the most recently validated version. Docker version: 17.06.1-ce. Max validated version: 1.12
+# [kubeadm] WARNING: starting in 1.8, tokens expire after 24 hours by default (if you require a non-expiring token use --token-ttl 0)
+# [certificates] Generated CA certificate and key.
+# [certificates] Generated API server certificate and key.
+# [certificates] API Server serving cert is signed for DNS names [cadu.vps.etica.ai kubernetes kubernetes.default kubernetes.default.svc kubernetes.default.svc.cluster.local] and IPs [10.96.0.1 149.56.101.234]
+# [certificates] Generated API server kubelet client certificate and key.
+# [certificates] Generated service account token signing key and public key.
+# [certificates] Generated front-proxy CA certificate and key.
+# [certificates] Generated front-proxy client certificate and key.
+# [certificates] Valid certificates and keys now exist in "/etc/kubernetes/pki"
+# [kubeconfig] Wrote KubeConfig file to disk: "/etc/kubernetes/admin.conf"
+# [kubeconfig] Wrote KubeConfig file to disk: "/etc/kubernetes/kubelet.conf"
+# [kubeconfig] Wrote KubeConfig file to disk: "/etc/kubernetes/controller-manager.conf"
+# [kubeconfig] Wrote KubeConfig file to disk: "/etc/kubernetes/scheduler.conf"
+# [apiclient] Created API client, waiting for the control plane to become ready
+
+
+
+### (3/4) Installing a pod network
+
+# Note: I will stop here. kubeadm Maturity is alpha, and other more stable
+#       strategies need bare metal servers or a AWS/Google Cloud/etc.
+#       Is possible, just need dedicated hardware (fititnt, 2017-08-26 02:57 BRT)
