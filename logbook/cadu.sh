4c957d3550c159d7af3f1d49e5cbe73a7aaff72e
Emerson Rocha Luiz <emerson@alligo.com.br>
2017-08-26 02:11:20 -0300

    cadu: following kubernetes tutorial, Using kubeadm to Create a Cluster~

--- logbook/cadu.sh
+++ logbook/cadu.sh
@@ -13,3 +13,43 @@ sudo apt upgrade
 sudo hostnamectl set-hostname cadu.vps.etica.ai
 
 sudo apt install htop
+
+#### Kubernetes on Ubuntu
+#
+### Using kubeadm to Create a Cluster
+### See https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
+#
+## Installing kubeadm
+## See https://kubernetes.io/docs/setup/independent/install-kubeadm/
+# Installing Docker
+# See https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+sudo docker run hello-world # Testing docker
+
+## Installing kubectl
+## See https://kubernetes.io/docs/tasks/tools/install-kubectl/
+sudo snap install kubectl --classic
+kubectl cluster-info # Test if enabled
+
+# Enabling shell autocompletion
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
