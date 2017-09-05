1a086ddff4d56ce77fa554d8e059c4d29753b6c7
Emerson Rocha Luiz <emerson@alligo.com.br>
2017-09-05 17:41:12 -0300

    tools/cadu: closes #2, tested and documented backup/restore scripts

--- tools/backup/cadu-backup.sh
+++ tools/backup/cadu-backup.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-
-# Cadu Backup
-
-# mkdir -p /backup/eticaai/cadu/temp # Create folder first
+#===============================================================================
+#
+#          FILE:  cadu-backup.sh
+#
+#         USAGE:  ./cadu-backup.sh
+#
+#   DESCRIPTION:  Backup cadu server (mongodb and dockerfiles configuration).
+#                 Stop MongoDB and Rocket.Chat, rsync cadu with local folder,
+#                 then start MongoDB and Rocket.Chat.
+#
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  Requires passwordless `ssh root@cadu.vps.etica.ai`.
+#                 Requires `mkdir -p /backup/eticaai/cadu/` 1st time
+#          BUGS:  ---
+#         NOTES:  TODO: convert to make backup without stoping the services
+#        AUTHOR:  Emerson Rocha <emerson@alligo.com.br>
+#       COMPANY:  Etica.AI
+#       VERSION:  1.0
+#       CREATED:  2017-09-05 00:54:00 BRT
+#      REVISION:  ---
+#===============================================================================
 
 echo "Stoping mongo and rocketchat"
 ssh root@cadu.vps.etica.ai "cd /root/config; docker-compose -f ./chat/docker-compose.yml stop rocketchat; docker-compose -f ./chat/docker-compose.yml stop mongo"
--- tools/backup/cadu-restore.sh
+++ tools/backup/cadu-restore.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-# Cadu Restore of Backup
-
-# TODO: this script should stop for any error (fititnt, 2017-09-05 01:08 BRT)
-
-
-# mkdir -p /backup/eticaai/cadu/temp # Create folder first
+#===============================================================================
+#
+#          FILE:  cadu-restore.sh
+#
+#         USAGE:  ./cadu-restore.sh
+#
+#   DESCRIPTION:  Restore a backup created with cadu-backup.sh.
+#                 Stop MongoDB and Rocket.Chat if necessary, then rsync local
+#                 folder with remote cadu /root/config folder, then start
+#                 MongoDB and Rocket.Chat.
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  Requires passwordless `ssh root@cadu.vps.etica.ai`.
+#                 Requires that cadu already has a working docker, with all
+#                 containers working, even if no content.
+#          BUGS:  TODO 1: this script should stop for any error (fititnt, 2017-09-05 01:08 BRT)
+#                 TODO 2: this script should only restore if local folder really have some content (avoid destroy remote)
+#         NOTES:  ---
+#        AUTHOR:  Emerson Rocha <emerson@alligo.com.br>
+#       COMPANY:  Etica.AI
+#       VERSION:  1.0
+#       CREATED:  2017-09-05 01:20:00 BRT
+#      REVISION:  ---
+#===============================================================================
 
 echo "Restore CADU server in 5 seconds..."
 
